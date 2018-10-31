8ae3577bb03f626fcbe8c5c0b575b0b34c2c59cf
sp6029 <sp6029@att.com>
2018-10-31 14:54:10 -0400

    feat: remove maple key from javascript file

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'pk.eyJ1Ijoic3B1Z2hhemhlbmRoaSIsImEiOiJjam5vcXR1dzIwMjUzM3FxczY5aGtqb2x2In0.8_kKOOjhD56Ea3oXyJSX_g',
+    mapboxToken: '',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
