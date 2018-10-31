e524266e2635b3c4826e06954044b9f66f404737
sp6029 <sp6029@att.com>
2018-10-31 10:51:52 -0400

    feat: adding aria attribute to ul element

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -133,7 +133,7 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
  */
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
-  const title = document.createElement('h2');
+  const title = document.createElement('h3');
   title.setAttribute('tabindex',0);
   title.innerHTML = 'Reviews';
   container.appendChild(title);
@@ -186,6 +186,7 @@ createReviewHTML = (review) => {
 fillBreadcrumb = (restaurant=self.restaurant) => {
   const breadcrumb = document.getElementById('breadcrumb');
   const li = document.createElement('li');
+  li.setAttribute("aria-current","page");
   li.innerHTML = restaurant.name;
   breadcrumb.appendChild(li);
 }
